15350f13ae46b608ecfd149421c8e0f5c43e16e7
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-03-10 12:50:15 +0900

    setup FB redux initialization

---
+++ src/Firebase/firebaseConfig.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCA8zAjA_sj_wzqt13n7MG2aNKYxONekuw",
+  authDomain: "solchall.firebaseapp.com",
+  projectId: "solchall",
+  storageBucket: "solchall.appspot.com",
+  messagingSenderId: "853182874292",
+  appId: "1:853182874292:web:118b95fa587ec666f78de1",
+  measurementId: "G-FJC6QR65LL",
+};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
