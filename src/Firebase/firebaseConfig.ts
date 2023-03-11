c076ed468b29237803ab9e3076e1610834009380
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-03-11 18:49:30 +0900

    #2 add: set FB config to env file

--- src/Firebase/firebaseConfig.ts
+++ src/Firebase/firebaseConfig.ts
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyCA8zAjA_sj_wzqt13n7MG2aNKYxONekuw",
-  authDomain: "solchall.firebaseapp.com",
-  projectId: "solchall",
-  storageBucket: "solchall.appspot.com",
-  messagingSenderId: "853182874292",
-  appId: "1:853182874292:web:118b95fa587ec666f78de1",
-  measurementId: "G-FJC6QR65LL",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
